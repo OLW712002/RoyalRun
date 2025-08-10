0d2df82f84930b59047b23f8b918e599630ddb23
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-08-10 14:14:33 +0700

    Update LevelGenerator
    Add moving for chunks

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,19 +5,25 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] GameObject chunkPrefab;
     [SerializeField] int startingChunkAmount = 12;
     [SerializeField] Transform chunkParent;
-    float chunkLength = 10f;
+    [SerializeField] float chunkLength = 10f;
+    [SerializeField] float chunkMoveSpeed = 10f;
+
+    GameObject[] chunks = new GameObject[12];
     
     void Start()
     {
         for (int i = 0; i < startingChunkAmount; i++)
         {
             Vector3 chunkPos = transform.position + i * chunkLength * Vector3.forward;
-            Instantiate(chunkPrefab, chunkPos, Quaternion.identity, chunkParent);
+            chunks[i] = Instantiate(chunkPrefab, chunkPos, Quaternion.identity, chunkParent);
         }
     }
 
     void Update()
     {
-        
+        for (int i = 0; i < chunks.Length; i++)
+        {
+            chunks[i].transform.Translate(Vector3.back * chunkMoveSpeed * Time.deltaTime);
+        }
     }
 }
