972c9e9d2c280bf27d8aab1114e7436f9afdcd25
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-08-11 15:54:56 +0700

    Update LevelGenerator.cs
    Remove chunk when it useless, and regenerate new chunk

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class LevelGenerator : MonoBehaviour
 {
@@ -8,22 +9,38 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] float chunkLength = 10f;
     [SerializeField] float chunkMoveSpeed = 10f;
 
-    GameObject[] chunks = new GameObject[12];
+    //GameObject[] chunks = new GameObject[12];
+    List<GameObject> chunks = new List<GameObject>();
     
     void Start()
     {
         for (int i = 0; i < startingChunkAmount; i++)
         {
             Vector3 chunkPos = transform.position + i * chunkLength * Vector3.forward;
-            chunks[i] = Instantiate(chunkPrefab, chunkPos, Quaternion.identity, chunkParent);
+            GameObject chunk = Instantiate(chunkPrefab, chunkPos, Quaternion.identity, chunkParent);
+            chunks.Add(chunk);
         }
     }
 
     void Update()
     {
-        for (int i = 0; i < chunks.Length; i++)
+        MoveChunks();
+    }
+
+    void MoveChunks()
+    {
+        for (int i = 0; i < chunks.Count; i++)
         {
+            GameObject chunk = chunks[i];
             chunks[i].transform.Translate(Vector3.back * chunkMoveSpeed * Time.deltaTime);
+            if (chunk.transform.position.z <= Camera.main.transform.position.z)
+            {
+                chunks.Remove(chunk);
+                Destroy(chunk);
+                Vector3 newChunkPos = transform.position + (startingChunkAmount * chunkLength + Camera.main.transform.position.z) * Vector3.forward;
+                GameObject newChunk = Instantiate(chunkPrefab, newChunkPos, Quaternion.identity, chunkParent);
+                chunks.Add(newChunk);
+            }
         }
     }
 }
