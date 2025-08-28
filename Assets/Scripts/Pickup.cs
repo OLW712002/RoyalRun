5ffa25df51f42e168eb914fce28794d869662ba1
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-08-28 18:14:51 +0700

    Update pickups

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 
-public class Pickup : MonoBehaviour
+public abstract class Pickup : MonoBehaviour
 {
+    [SerializeField] float rotateSpeed = 100f;
+
     const string playerTag = "Player";
+
+    private void Update()
+    {
+        transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f);
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(playerTag))
         {
-            Debug.Log(other.gameObject.name);
+            OnPickup();
+            Destroy(gameObject);
         }
     }
+
+    protected abstract void OnPickup();
 }
