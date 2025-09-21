c393b9c3140e0284db1c281b16bc823c664e0aaa
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-09-21 22:41:05 +0700

    Add move limit boundary

---
+++ Assets/Scripts/Player/PlayerMoveRange.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMoveRange : MonoBehaviour
+{
+    [SerializeField] Material quadMaterial;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
