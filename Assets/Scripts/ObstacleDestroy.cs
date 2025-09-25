fc72581857dba7902505614f44f9bbd0eedb033f
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-09-25 22:21:09 +0700

    Update obstacle destroy
    if stuck, destroy it

--- Assets/Scripts/ObstacleDestroy.cs
+++ Assets/Scripts/ObstacleDestroy.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ObstacleDestroy : MonoBehaviour
 {
     ObstacleSpawner obstacleSpawner;
+    Transform lastTrackingPos;
 
     public void Init(ObstacleSpawner os)
     {
@@ -16,7 +17,16 @@ public class ObstacleDestroy : MonoBehaviour
 
     void CheckObstaclePos()
     {
-        if (transform.position.z < Camera.main.transform.position.z - 5 || transform.position.y < -10)
+        if (lastTrackingPos == null)
+        {
+            Debug.Log("null");
+            lastTrackingPos = this.transform;
+            return;
+        }
+        bool isOutOfCameraView = transform.position.z < Camera.main.transform.position.z - 5 || transform.position.y < -10;
+        bool isStuck = lastTrackingPos.position.z - transform.position.z < 5;
+
+        if (isOutOfCameraView || isStuck)
         {
             CancelInvoke("CheckObstaclePos");
             obstacleSpawner.DecreaseObstacleInScene();
